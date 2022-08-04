a7b84d09716dbad1fd3510fc166b159c52b7af0b
Daniel Adams <itseni@dandabs.fi>
2022-08-04 15:02:11 +0100

    Added new form

--- ASProj/Games/frmSwim.Designer.cs
+++ ASProj/Games/frmSwim.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmSwim
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1064, 671);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSwim";
             this.Text = "frmSwim";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
